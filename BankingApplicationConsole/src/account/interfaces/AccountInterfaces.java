93b8a8fb4d0d70547405a8b223e5b2af6423f536
Kamalesh G <kamalesh@example.com>
2025-05-19 22:23:09 +0530

    BankingApplication

---
+++ BankingApplicationConsole/src/account/interfaces/AccountInterfaces.java
@@ -0,0 +1,10 @@
+package account.interfaces;
+
+import account.model.ACCreationModel;
+import account.model.LoginModel;
+
+public interface AccountInterfaces {
+    public String registerAccount(ACCreationModel model);
+    public String loginAccount(LoginModel model);
+    public double getBalance(LoginModel model);
+}
