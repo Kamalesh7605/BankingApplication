8a633aedb0ffd69bf07bb85c6f269b84ed81e702
Kamalesh G <kamalesh@example.com>
2025-05-20 09:30:51 +0530

    Interface changes

--- BankingApplicationConsole/src/account/interfaces/AccountInterfaces.java
+++ BankingApplicationConsole/src/account/interfaces/AccountInterfaces.java
@@ -7,4 +7,6 @@ public interface AccountInterfaces {
     public String registerAccount(ACCreationModel model);
     public String loginAccount(LoginModel model);
     public double getBalance(LoginModel model);
+    public String withdraw(double amount, LoginModel model);
+    public String deposit(double amount, LoginModel model);
 }
